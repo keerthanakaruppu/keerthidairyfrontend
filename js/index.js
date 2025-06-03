95356dc50aba91447989ef16b98e40ac3ae63aa2
keerthanakaruppu <keerthanakaruppu111@gmail.com>
2025-06-03 18:40:20 +0530

    initial deploy

--- js/index.js
+++ js/index.js
@@ -1,21 +1,25 @@
-const form = document.getElementById("loginForm");
-form.addEventListener("submit", async (e) => {
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
   e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("https://keerthidairybackend.onrender.com/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password })
-  });
+  try {
+    const res = await fetch("https://keerthidairybackend.onrender.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ email, password }),
+    });
 
-  const data = await res.json();
-
-  if (data.success) {
-    sessionStorage.setItem("auth", "true");
-    window.location.href = "gallery.html";
-  } else {
-    alert("Invalid email or password");
+    const data = await res.json();
+    if (res.ok && data.success) {
+      location.href = "gallery.html";
+    } else {
+      alert("Invalid email or password");
+    }
+  } catch (err) {
+    alert("Error logging in");
   }
 });
