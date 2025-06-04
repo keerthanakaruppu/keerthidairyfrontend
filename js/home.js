e53a09e183d2fa9a72885c8f6a3bf15913323d59
keerthanakaruppu <keerthanakaruppu111@gmail.com>
2025-06-04 20:04:28 +0530

    initial deploy

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 // typscript function
 let typing = new Typed(".text", {
     strings: ["Kabaddi Player...", "Kho-Kho Player...", "Dancer...","Athelete..."],
-    typespeed: 900,
-    backspeed: 400,
+    typespeed: 1000,
+    backspeed: 1000,
     backDelay: 1000,
     loop: true,
   });
